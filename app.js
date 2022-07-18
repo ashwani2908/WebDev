41b70c93e869ac9be69396b1e11c7e59612b051a
ashwani2908 <76448715+ashwani2908@users.noreply.github.com>
2022-07-18 23:57:43 +0530

    Doms Task 4 getElementsByClassName()

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.forms);
-console.log(document.links);
-console.log(document.forms[0]);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.forms[0]);
 
-console.log(document.images);
+// console.log(document.images);
 
 
 
-var headerTitle =document.getElementById('header-title');
-var header = document.getElementById('main-header');
-console.log(headerTitle);
+// var headerTitle =document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headerTitle);
 
-header.style.borderBottom ='solid 3px #000'
+// header.style.borderBottom ='solid 3px #000'
 
-//headerTitle.textContent = 'Hello';
-//headerTitle.innerText = 'goodbye';
+// //headerTitle.textContent = 'Hello';
+// //headerTitle.innerText = 'goodbye';
 
-var items = document.getElementsByClassName('title');
-//console.log(items);
-items[0].style.color = "green";
-items[0].style.fontWeight = '900';
+// var items = document.getElementsByClassName('title');
+// //console.log(items);
+// items[0].style.color = "green";
+// items[0].style.fontWeight = '900';
+
+// document.getElementsByClassName()
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+
+items[2].style.backgroundColor = 'green';
+
+for(let i=0 ; i< items.length ; i++){
+    items[i].style.fontWeight = 'bold';
+}

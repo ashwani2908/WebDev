406e4a8bf23fbf85ef1c7c3c03d46ef3491df8bf
ashwani2908 <76448715+ashwani2908@users.noreply.github.com>
2022-07-26 16:34:34 +0530

    Doms Remove and Edit TaSK 8

---
+++ delete.js
@@ -0,0 +1,20 @@
+
+var itemList = document.getElementById('items');
+
+
+
+
+// Delete event
+itemList.addEventListener('click', removeItem);
+
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+

c43afb9ba97ec5c0c769fe6d700460aa8956e509
ashwani2908 <76448715+ashwani2908@users.noreply.github.com>
2023-03-30 20:38:17 +0530

    Edit functionality

--- Appointment Booking App/app.js
+++ Appointment Booking App/app.js
@@ -14,7 +14,7 @@ function submitBtn(e) {
 
         // let stringObj = JSON.stringify(object);
         // localStorage.setItem('userDetail'+ emailId, stringObj);
-        axios.post("https://crudcrud.com/api/7f1d57d17cbf44258aa81d8a0c4cda11/appointmentapp", object)
+        axios.post("https://crudcrud.com/api/133320e8c0ff4cec88d7164724ae811c/appointmentapp", object)
             .then(res => {
                 addNewDetail(res.data);
                 console.log(res.data);
@@ -26,7 +26,7 @@ function submitBtn(e) {
 };
 
 window.addEventListener("DOMContentLoaded", () =>{
-    axios.get("https://crudcrud.com/api/7f1d57d17cbf44258aa81d8a0c4cda11/appointmentapp")
+    axios.get("https://crudcrud.com/api/133320e8c0ff4cec88d7164724ae811c/appointmentapp")
             .then(res => {
                 //console.log(res.data)
                 for(var i=0; i< res.data.length;i++){
@@ -49,6 +49,10 @@ function addNewDetail(object){
         editBtn.addEventListener('click', ()=>{
             document.getElementById('name').value = object.name;
             document.getElementById('email').value = object.emailId;
+            axios.delete(`https://crudcrud.com/api/133320e8c0ff4cec88d7164724ae811c/appointmentapp/${object._id}`)
+            .then(res => {
+                console.log(res)
+            }).catch(err => console.log(err))
             li.remove();
         });
 
@@ -61,7 +65,7 @@ function addNewDetail(object){
         deleteBtn.type = 'button';
         deleteBtn.value = 'Delete';
         deleteBtn.addEventListener('click', ()=>{
-            axios.delete(`https://crudcrud.com/api/7f1d57d17cbf44258aa81d8a0c4cda11/appointmentapp/${object._id}`)
+            axios.delete(`https://crudcrud.com/api/133320e8c0ff4cec88d7164724ae811c/appointmentapp/${object._id}`)
             .then(res => {
                 console.log(res)
             }).catch(err => console.log(err))

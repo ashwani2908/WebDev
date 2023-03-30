94e3da602ca934ff9bc8ef46c40e02e0947c16fb
ashwani2908 <76448715+ashwani2908@users.noreply.github.com>
2023-03-30 15:59:58 +0530

    Added delete functionality
    use axios.delete the appointment by selecting _id

--- Appointment Booking App/app.js
+++ Appointment Booking App/app.js
@@ -14,7 +14,7 @@ function submitBtn(e) {
 
         // let stringObj = JSON.stringify(object);
         // localStorage.setItem('userDetail'+ emailId, stringObj);
-        axios.post("https://crudcrud.com/api/ba09eebf70d64789bf84f027ccd48e04/appointmentapp", object)
+        axios.post("https://crudcrud.com/api/7f1d57d17cbf44258aa81d8a0c4cda11/appointmentapp", object)
             .then(res => {
                 addNewDetail(res.data);
                 console.log(res.data);
@@ -26,10 +26,11 @@ function submitBtn(e) {
 };
 
 window.addEventListener("DOMContentLoaded", () =>{
-    axios.get("https://crudcrud.com/api/ba09eebf70d64789bf84f027ccd48e04/appointmentapp")
+    axios.get("https://crudcrud.com/api/7f1d57d17cbf44258aa81d8a0c4cda11/appointmentapp")
             .then(res => {
-                console.log(res.data)
+                //console.log(res.data)
                 for(var i=0; i< res.data.length;i++){
+                    // console.log(res.data[i])
                     addNewDetail(res.data[i])
                 }
             }).catch(err => console.log(err))
@@ -60,14 +61,17 @@ function addNewDetail(object){
         deleteBtn.type = 'button';
         deleteBtn.value = 'Delete';
         deleteBtn.addEventListener('click', ()=>{
-            localStorage.removeItem('userDetail'+ object.emailId);
+            axios.delete(`https://crudcrud.com/api/7f1d57d17cbf44258aa81d8a0c4cda11/appointmentapp/${object._id}`)
+            .then(res => {
+                console.log(res)
+            }).catch(err => console.log(err))
             li.remove();
         });
         
         li.appendChild(deleteBtn);
 
 
-    console.log(li);
+    
     ul.appendChild(li);
 
 };
